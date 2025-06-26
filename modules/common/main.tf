bcab4bbdf179857778d6b21976b3f6d0c51b3a98
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-06-26 20:51:03 +0900

    ローカル変数と関連する出力を削除し、コードの整理を実施。

--- modules/common/main.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  github_owner_name = "k-kazuya0926"
-  project_name      = "my-modern-application-sample"
-}
--- modules/common/output.tf
+++
@@ -1,7 +0,0 @@
-output "github_owner_name" {
-  value = local.github_owner_name
-}
-
-output "project_name" {
-  value = local.project_name
-}
