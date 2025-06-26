862e727a95b7350e473f747941bed04419302e19
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-06-27 06:28:01 +0900

    GitHub Actions用のOIDCプロバイダーをモジュール化し、関連するIAMロールとポリシーを新しいモジュールに移動。

--- environments/prod/aws_iam.tf
+++ environments/prod/aws_iam.tf
@@ -55,51 +55,12 @@ data "aws_iam_policy_document" "tmp" {
 }
 
 
-resource "aws_iam_openid_connect_provider" "github_actions" {
-  url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1",
-    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
-  ]
-}
-
-resource "aws_iam_role" "github_actions" {
-  name               = "${var.project_name}-${var.env}-github-actions"
-  assume_role_policy = data.aws_iam_policy_document.github_actions_assume.json
-}
-
-data "aws_iam_policy_document" "github_actions_assume" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-
-    principals {
-      type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
-    }
-
-    condition {
-      test     = "StringLike"
-      variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_owner_name}/${var.project_name}:*"]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:aud"
-      values   = ["sts.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_policy" "github_actions" {
-  name   = "${var.project_name}-${var.env}-github-actions"
-  policy = data.aws_iam_policy_document.github_actions.json
+module "github_actions_openid_connect_provider" {
+  source            = "../../modules/github_actions_openid_connect_provider"
+  github_owner_name = var.github_owner_name
+  project_name      = var.project_name
+  env               = var.env
+  policy            = data.aws_iam_policy_document.github_actions.json
 }
 
 data "aws_iam_policy_document" "github_actions" {
@@ -147,9 +108,4 @@ data "aws_iam_policy_document" "github_actions" {
       "arn:aws:lambda:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:function:${var.project_name}-${var.env}-tmp:*"
     ]
   }
-}
-
-resource "aws_iam_role_policy_attachment" "github_actions" {
-  role       = aws_iam_role.github_actions.name
-  policy_arn = aws_iam_policy.github_actions.arn
 }
---
+++ modules/github_actions_openid_connect_provider/variables.tf
@@ -0,0 +1,16 @@
+variable "github_owner_name" {
+  description = "GitHub のオーナー名"
+  type        = string
+}
+
+variable "project_name" {
+  description = "プロジェクト名"
+  type        = string
+}
+
+variable "env" {
+  description = "環境"
+  type        = string
+}
+
+variable "policy" {}
