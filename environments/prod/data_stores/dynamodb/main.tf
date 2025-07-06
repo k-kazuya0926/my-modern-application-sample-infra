c514af35fb11529c90165667cd27c11453fe73b3
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-07-06 14:43:06 +0900

    DynamoDBに新しいテーブル「mail_addresses」を追加し、初期アイテムを設定。メールアドレスをハッシュキーとして使用し、エラーフラグを管理するためのグローバルセカンダリインデックスを定義。

--- environments/prod/data_stores/dynamodb/main.tf
+++ environments/prod/data_stores/dynamodb/main.tf
@@ -44,3 +44,115 @@ module "dynamodb_sequences_table" {
     }
   ]
 }
+
+module "dynamodb_mail_addresses_table" {
+  source = "../../../../modules/dynamodb"
+
+  github_repository_name      = var.github_repository_name
+  env                         = local.env
+  table_name                  = "mail_addresses"
+  hash_key                    = "email"
+  deletion_protection_enabled = false # サンプルであるため無効に設定
+
+  attributes = [
+    {
+      name = "email"
+      type = "S"
+    },
+    {
+      name = "has_error"
+      type = "N"
+    },
+  ]
+
+  global_secondary_indexes = [
+    {
+      name            = "has_error-index"
+      hash_key        = "has_error"
+      projection_type = "ALL"
+    },
+  ]
+
+  initial_items = [
+    {
+      item = {
+        email = {
+          S = "success@simulator.amazonses.com"
+        }
+        user_name = {
+          S = "success"
+        }
+        has_error = {
+          N = "0"
+        }
+        is_sent = {
+          N = "0"
+        }
+      }
+    },
+    {
+      item = {
+        email = {
+          S = "bounce@simulator.amazonses.com"
+        }
+        user_name = {
+          S = "bounce"
+        }
+        has_error = {
+          N = "0"
+        }
+        is_sent = {
+          N = "0"
+        }
+      }
+    },
+    {
+      item = {
+        email = {
+          S = "ooto@simulator.amazonses.com"
+        }
+        user_name = {
+          S = "ooto"
+        }
+        has_error = {
+          N = "0"
+        }
+        is_sent = {
+          N = "0"
+        }
+      }
+    },
+    {
+      item = {
+        email = {
+          S = "complaint@simulator.amazonses.com"
+        }
+        user_name = {
+          S = "complaint"
+        }
+        has_error = {
+          N = "0"
+        }
+        is_sent = {
+          N = "0"
+        }
+      }
+    },
+    {
+      item = {
+        email = {
+          S = "suppressionlist@simulator.amazonses.com"
+        }
+        user_name = {
+          S = "suppressionlist"
+        }
+        has_error = {
+          N = "0"
+        }
+        is_sent = {
+          N = "0"
+        }
+      }
+    }
+  ]
+}
