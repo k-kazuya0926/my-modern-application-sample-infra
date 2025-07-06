30b958b795ed43905b1a8ad0205c279da278e1c8
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-07-06 09:32:05 +0900

    不要な変数「github_owner_name」を削除し、DynamoDBの設定を整理。

--- environments/prod/data_stores/dynamodb/variables.tf
+++ environments/prod/data_stores/dynamodb/variables.tf
@@ -1,8 +1,3 @@
-variable "github_owner_name" {
-  type        = string
-  description = "GitHubオーナー名"
-}
-
 variable "github_repository_name" {
   type        = string
   description = "GitHubリポジトリ名"
