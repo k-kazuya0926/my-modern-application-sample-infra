138e200182728e39d2160f38569da7127e5ccf10
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-07-07 06:47:11 +0900

    新しいECRモジュール「send-message」を追加し、GitHubリポジトリ名と環境変数を設定。これにより、メッセージ送信用のECRリポジトリが構成される。

--- environments/prod/ecr.tf
+++ environments/prod/ecr.tf
@@ -25,3 +25,10 @@ module "ecr_register_user" {
   env                    = local.env
   ecr_repository_name    = "register-user"
 }
+
+module "ecr_send_message" {
+  source                 = "../../modules/ecr"
+  github_repository_name = var.github_repository_name
+  env                    = local.env
+  ecr_repository_name    = "send-message"
+}
