6344e903e217c9bf7f66049e6be2397bfe411e62
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-07-13 18:20:43 +0900

    API GatewayにCognito認証用の新しいモジュール「api_gateway_auth_by_cognito」を追加。これにより、Cognitoを使用した認証APIが実装され、ルートおよび統合設定が追加されました。

--- environments/prod/api_gateway.tf
+++ environments/prod/api_gateway.tf
@@ -25,3 +25,31 @@ module "api_gateway_register_user" {
     }
   ]
 }
+
+module "api_gateway_auth_by_cognito" {
+  source                 = "../../modules/api_gateway_http"
+  github_repository_name = var.github_repository_name
+  env                    = local.env
+  api_name               = "auth-by-cognito"
+  description            = "Cognito authentication API"
+
+  routes = [
+    {
+      route_key = "POST /auth-by-cognito"
+    }
+  ]
+
+  integrations = [
+    {
+      integration_uri    = module.lambda_auth_by_cognito.function_arn
+      integration_type   = "AWS_PROXY"
+      integration_method = "POST"
+    }
+  ]
+
+  lambda_permissions = [
+    {
+      function_name = module.lambda_auth_by_cognito.function_name
+    }
+  ]
+}
