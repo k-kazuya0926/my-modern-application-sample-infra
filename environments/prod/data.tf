ae07938c04894fad00bcb29abaf0001511a824bd
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-07-24 07:27:22 +0900

    Terraformのプロダクション環境用のDynamoDBの状態ファイルのキーを環境変数に基づいて動的に設定するように変更しました。

--- environments/prod/data.tf
+++ environments/prod/data.tf
@@ -5,7 +5,7 @@ data "terraform_remote_state" "dynamodb" {
   backend = "s3"
   config = {
     bucket = "${var.github_repository_name}-${local.env}-tfstate"
-    key    = "prod/data_stores/dynamodb/terraform.tfstate"
+    key    = "${local.env}/data_stores/dynamodb/terraform.tfstate"
     region = "ap-northeast-1"
   }
 }
