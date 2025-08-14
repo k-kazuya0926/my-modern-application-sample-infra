95211d3600a7051693e01d149b4465869c9db1ba
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-08-14 20:26:44 +0900

    Google Cloudの基本設定を追加。Terraformのプロバイダー設定、Compute Engineインスタンスのリソース定義、プロジェクトIDの変数を新規作成。

---
+++ google_cloud/basic/main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  project = var.project_id
+  region  = "asia-northeast1"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-micro"
+  zone         = "asia-northeast1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
