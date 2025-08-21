067fb8f3838f84d8c66ed73d08e100856ac4c874
kazuya.kobayashi <kobakazu3686@gmail.com>
2025-08-22 07:26:19 +0900

    GKEクラスタの環境を「prod」から「dev」に変更

--- google/modules/gke_cluster/examples/main.tf
+++ google/modules/gke_cluster/examples/main.tf
@@ -4,5 +4,5 @@ module "gke_cluster" {
   service_name    = var.service_name
   billing_account = var.billing_account
   service_viewers = []
-  environment     = "prod"
+  environment     = "dev"
 }
